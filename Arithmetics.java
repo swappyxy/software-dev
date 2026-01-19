7be3bf84dfc193235ffa209e32e82bc29f3eb2f8
swappyxy <shelditd@gmail.com>
2026-01-19 13:51:27 +0000

    new file:   Arithmetics.java

---
+++ Arithmetics.java
@@ -0,0 +1,67 @@
+import java.util.Scanner;
+
+public class Arithmetics {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        int first, second, third, result;
+        System.out.print("Enter first number: ");
+        first = input.nextInt();
+        System.out.print("Enter second number: ");
+        second = input.nextInt();
+        System.out.print("Enter third number: ");
+        third = input.nextInt();
+        
+        result = first+second;
+        System.out.println(first +" + " + second + " = " + result );
+
+        third=10;
+        
+        System.out.println(result);
+
+            result+=third;
+            System.out.println( " + " + third + " = " + result);
+         
+        int number = 0;
+        System.out.println(number);
+        number++;
+        System.out.println(number++);
+
+        System.out.println(number);
+        number+=2;
+         System.out.println(number);
+        input.close();  
+
+
+number+=4;
+        System.out.println(number);
+        number-=2;
+        System.out.println(number);
+        number*=3;
+        System.out.println(number);
+        number/=4;
+        System.out.println(number);
+        number-=2;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+ }
+}
