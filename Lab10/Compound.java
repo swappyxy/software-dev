a109e90ab05409bbbe24a7202e37bbb6ef9b7d06
swappyxy <shelditd@gmail.com>
2026-01-21 15:45:29 +0000

    modified:   Arithmetics.java 	new file:   Lab10/Compound.java 	new file:   Lab10/EvenOdd.java

---
+++ Lab10/Compound.java
@@ -0,0 +1,14 @@
+package Lab10;
+import java.util.Scanner;
+public class Compound {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        System.out.print("Enter a number: ");
+        int number = input.nextInt();
+        number += 2;
+        System.out.println("After adding 2: " + number);
+        number -= 5;
+        System.out.println("After subtracting 5: " + number);
+
+    }
+}
---
+++ Lab10/EvenOdd.java
@@ -0,0 +1,22 @@
+package Lab10;
+import java.util.Scanner;
+
+public class EvenOdd {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        
+        System.out.print("Enter a number: ");
+        int number = scanner.nextInt();
+        
+        int remainder = number;
+        remainder %= 2;
+        
+        if (remainder == 0) {
+            System.out.println(number + " is even.");
+        } else {
+            System.out.println(number + " is odd.");
+        }
+        
+        scanner.close();
+    }
+}
